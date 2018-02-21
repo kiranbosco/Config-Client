354e5ed8fb3141a579c93876437c5ad753e274e3
Superman computers <Superman computers@DESKTOP-AGHAT19>
2018-02-21 14:11:10 +0530

    New Client APP

---
+++ Config-Client/src/main/java/com/example/demo/ConfigClientApplication.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class ConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClientApplication.class, args);
+	}
+	
+	@RestController
+	@RefreshScope
+	class MessageService{
+		
+		@Value("${message}")
+		String message;
+
+}
+}
